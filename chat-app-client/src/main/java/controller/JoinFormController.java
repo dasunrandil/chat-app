932bef09e3e964d63bbb83746effbc7cd66c254f
dasunrandil <dasunrandil@gmail.com>
2022-10-26 08:11:20 +0530

    Close #2

--- chat-app-client/src/main/java/controller/JoinFormController.java
+++ chat-app-client/src/main/java/controller/JoinFormController.java
@@ -1,4 +1,10 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
 public class JoinFormController {
+    public JFXTextField txtServerIP;
+    public JFXTextField txtName;
+    public JFXButton btnJoin;
 }
--- chat-app-client/src/main/java/controller/MainFormController.java
+++ chat-app-client/src/main/java/controller/MainFormController.java
@@ -1,4 +1,12 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextArea;
+
 public class MainFormController {
+    public JFXTextArea txtOutput;
+    public JFXTextArea txtMessage;
+    public JFXButton btnSend;
+    public JFXListView lstUsers;
 }
